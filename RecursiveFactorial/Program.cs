e929e6a88faedee43e0081e4e574b9ddc0747e70
Ronye do Lago Cornetti Rocha <ronye.rocha@sistemasseguros.local>
2018-02-26 13:40:35 -0300

    Factorial

--- RecursiveFactorial/Program.cs
+++ RecursiveFactorial/Program.cs
@@ -10,6 +10,19 @@ namespace RecursiveFactorial
     {
         static void Main(string[] args)
         {
+            Console.Write("Fatorial de: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n" + Factorial(n));
+            Console.ReadKey();
+        }
+
+        public static int Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * Factorial(n - 1);
         }
     }
 }
